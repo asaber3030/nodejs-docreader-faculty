bc9685f8591a68a151e1462000be90125b77707b
Abdulrahman Saber <abdulrahmansaber120@gmail.com>
2024-08-22 13:30:44 +0300

    update package.jso

--- src/utlis/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TAKE_LIMIT = exports.AUTH_COOKIE_ID = void 0;
-exports.AUTH_COOKIE_ID = "faculty-education-system-token";
-exports.TAKE_LIMIT = 10;
