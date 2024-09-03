6377a6b4eac7c67bcc6d8a48dcce37bed567a815
Abdulrahman Saber <abdulrahmansaber120@gmail.com>
2024-09-03 15:25:13 +0300

    Change: db host to aws

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express"
 import dotenv from "dotenv"
-import cors from "cors"
 import db from "../utlis/db"
+import cors from "cors"
 
 import { authRouter, subjectRouter, facultiesDataRouter, userRouter, facultyRouter, moduleRouter, lecturesRouter, yearRouter } from "../routes"
 
@@ -14,7 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", async (req: Request, res: Response) => {
-  return res.json({ message: "DocReader Guide - API", status: "Running" })
+  //return res.json({ message: "DocReader Guide - API", status: "Running" })
 })
 
 app.use("/api/v1", [authRouter, facultiesDataRouter, userRouter, facultyRouter, moduleRouter, subjectRouter, lecturesRouter, yearRouter])
