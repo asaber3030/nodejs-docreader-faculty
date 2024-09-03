799cbca5ab0df2d6bdbdb96bfe4e79574fe35484
Abdulrahman Saber <abdulrahmansaber120@gmail.com>
2024-09-03 14:16:23 +0300

    Change database host & backup current database

--- api/index.ts
+++ api/index.ts
@@ -1,35 +1,24 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
-import cors from 'cors'
+import express, { Request, Response } from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import db from "../utlis/db"
 
-import { authRouter, subjectRouter, facultiesDataRouter, userRouter, facultyRouter, moduleRouter, lecturesRouter, yearRouter } from "../routes";
+import { authRouter, subjectRouter, facultiesDataRouter, userRouter, facultyRouter, moduleRouter, lecturesRouter, yearRouter } from "../routes"
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 const port = process.env.APP_PORT || 8080
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({
-    message: "DocReader API - For Faculty",
-    status: "Running..."
-  });
-});
+app.get("/", async (req: Request, res: Response) => {
+  return res.json({ message: "DocReader Guide - API", status: "Running" })
+})
 
-app.use('/api/v1', [
-  authRouter,
-  facultiesDataRouter,
-  userRouter,
-  facultyRouter,
-  moduleRouter,
-  subjectRouter,
-  lecturesRouter,
-  yearRouter
-])
+app.use("/api/v1", [authRouter, facultiesDataRouter, userRouter, facultyRouter, moduleRouter, subjectRouter, lecturesRouter, yearRouter])
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
