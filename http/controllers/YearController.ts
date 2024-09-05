2b0eeee5eff661e7e1d6778ce4e24f253b32d77c
yosefbeder <dryosefbeder@gmail.com>
2024-09-05 21:49:32 +0300

    Improv e error handling for the years route

--- http/controllers/YearController.ts
+++ http/controllers/YearController.ts
@@ -5,41 +5,65 @@ import { send } from "../../utlis/responses";
 
 export default class YearController {
   async getSubjects(req: Request, res: Response) {
-    const yearId = +req.params.yearId;
-    const subjects = await db.$queryRawUnsafe(
-      `${subjectQuery} WHERE m."yearId" = $1`,
-      yearId
-    );
-    return send(res, "Year subjects", 200, subjects);
+    try {
+      const yearId = +req.params.yearId;
+      const subjects = await db.$queryRawUnsafe(
+        `${subjectQuery} WHERE m."yearId" = $1`,
+        yearId
+      );
+      return send(res, "Year subjects", 200, subjects);
+    } catch (errorObject) {
+      return res.status(500).json({
+        errorObject,
+        message: "Error - Something Went Wrong.",
+        status: 500,
+      });
+    }
   }
 
   async getLectures(req: Request, res: Response) {
-    const yearId = +req.params.yearId;
-    const { search, limit, offset } = req.query;
+    try {
+      const yearId = +req.params.yearId;
+      const { search, limit, offset } = req.query;
 
-    const searchPattern = search ? `%${search}%` : `%`;
-    let limitNum, offsetNum;
-    if (limit) limitNum = +limit;
-    else limitNum = Number.POSITIVE_INFINITY;
-    if (offset) offsetNum = +offset;
-    else offsetNum = 0;
+      const searchPattern = search ? `%${search}%` : `%`;
+      let limitNum, offsetNum;
+      if (limit) limitNum = +limit;
+      else limitNum = Number.POSITIVE_INFINITY;
+      if (offset) offsetNum = +offset;
+      else offsetNum = 0;
 
-    const lectures = await db.$queryRawUnsafe(
-      `${lectureQuery} WHERE m."yearId" = $1 AND LOWER(l.title) LIKE LOWER($2) LIMIT $3 OFFSET $4`,
-      yearId,
-      searchPattern,
-      limitNum,
-      offsetNum
-    );
-    return send(res, "Year lectures", 200, lectures);
+      const lectures = await db.$queryRawUnsafe(
+        `${lectureQuery} WHERE m."yearId" = $1 AND LOWER(l.title) LIKE LOWER($2) LIMIT $3 OFFSET $4`,
+        yearId,
+        searchPattern,
+        limitNum,
+        offsetNum
+      );
+      return send(res, "Year lectures", 200, lectures);
+    } catch (errorObject) {
+      return res.status(500).json({
+        errorObject,
+        message: "Error - Something Went Wrong.",
+        status: 500,
+      });
+    }
   }
 
   async getLinks(req: Request, res: Response) {
-    const yearId = +req.params.yearId;
-    const links = await db.$queryRawUnsafe(
-      `${linkQuery} WHERE m."yearId" = $1`,
-      yearId
-    );
-    return send(res, "Year links", 200, links);
+    try {
+      const yearId = +req.params.yearId;
+      const links = await db.$queryRawUnsafe(
+        `${linkQuery} WHERE m."yearId" = $1`,
+        yearId
+      );
+      return send(res, "Year links", 200, links);
+    } catch (errorObject) {
+      return res.status(500).json({
+        errorObject,
+        message: "Error - Something Went Wrong.",
+        status: 500,
+      });
+    }
   }
 }
