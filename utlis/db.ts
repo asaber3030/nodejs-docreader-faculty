4e5880a751fba3b77c0be3996cf3e8cf6f70ff9f
yosefbeder <dryosefbeder@gmail.com>
2024-09-05 19:01:41 +0300

    Remigrate to Neon PostgreSQL

--- utlis/db.ts
+++ utlis/db.ts
@@ -14,23 +14,23 @@ interface LinkWithPath extends LectureLink {
 }
 
 export const linkQuery = `SELECT
-  ll.id, ll.title, ll.subTitle, ll.url, ll.type,
-  ll.category, ll.createdAt, ll.updatedAt,
-  m.semesterName, m.id AS moduleId, m.name AS moduleName,
-  l.subjectId, s.name AS subjectName, ll.lectureId,
-  l.title AS lectureTitle
+	ll.id, ll.title, ll."subTitle", ll.url, ll.type,
+	ll.category, ll."createdAt", ll."updatedAt",
+	m."semesterName", m.id AS "moduleId", m.name AS "moduleName",
+  l."subjectId", s.name AS "subjectName", ll."lectureId",
+  l.title AS "lectureTitle"
 FROM
-  LectureLink ll
+	"LectureLink" ll
 JOIN
-  Lecture l ON ll.lectureId = l.id
+	"Lecture" l ON ll."lectureId" = l.id
 JOIN
-  Subject s ON l.subjectId = s.id
+	"Subject" s ON l."subjectId" = s.id
 JOIN
-  Module m ON s.moduleId = m.id`;
+	"Module" m ON s."moduleId" = m.id`;
 
 export async function findLinkMany(key: string, value: string | number): Promise<LinkWithPath[]> {
   return db.$queryRawUnsafe(
-    `${linkQuery} WHERE ll.${key} = ?`,
+    `${linkQuery} WHERE ll."${key}" = $1`,
     value
   )
 }
@@ -48,19 +48,19 @@ interface LectureWithPath extends Lecture {
 }
 
 export const lectureQuery = `SELECT
-  l.id, l.title, l.subTitle, l.type, l.date, l.createdAt, l.updatedAt,
-  m.semesterName, m.id AS moduleId, m.name AS moduleName,
-  l.subjectId, s.name AS subjectName
+	l.id, l.title, l."subTitle", l.type, l.date, l."createdAt", l."updatedAt",
+  m."semesterName", m.id AS "moduleId", m.name AS "moduleName",
+  l."subjectId", s.name AS "subjectName"
 FROM
-  Lecture l
+	"Lecture" l
 JOIN
-  Subject s ON l.subjectId = s.id
+	"Subject" s ON l."subjectId" = s.id
 JOIN
-  Module m ON s.moduleId = m.id`
+	"Module" m ON s."moduleId" = m.id`
 
 export async function findLectureMany(key: string, value: string | number): Promise<LectureWithPath[]> {
   return db.$queryRawUnsafe(
-    `${lectureQuery} WHERE l.${key} = ? ORDER BY l.date`,
+    `${lectureQuery} WHERE l."${key}" = $1 ORDER BY l.date`,
     value
   )
 }
@@ -76,16 +76,16 @@ interface SubjectWithPath extends Subject {
 }
 
 export const subjectQuery = `SELECT
-  s.id, s.name, s.icon, s.createdAt, s.updatedAt,
-  m.semesterName, m.id AS moduleId, m.name AS moduleName
+	s.id, s.name, s.icon, s."createdAt", s."updatedAt",
+  m."semesterName", m.id AS "moduleId", m.name AS "moduleName"
 FROM
-  Subject s
+	"Subject" s
 JOIN
-  Module m ON s.moduleId = m.id`
+	"Module" m ON s."moduleId" = m.id`
 
 export async function findSubjectMany(key: string, value: string | number): Promise<SubjectWithPath[]> {
   return db.$queryRawUnsafe(
-    `${subjectQuery} WHERE s.${key} = ?`,
+    `${subjectQuery} WHERE s."${key}" = $1`,
     value
   )
 }
